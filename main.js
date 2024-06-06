8b114c0989ff5cbdc503dc704f21f92db0fbc7b4
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-06 16:54:06 +0200

    Commenti in italiano dei passaggi da realizzare

---
+++ main.js
@@ -0,0 +1,19 @@
+// Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+// creare un circolo che generi i numeri da 1 a 100
+
+    //individua i numeri divisibili per 3
+
+        // cambia il valore dei numeri divisibili per 3 in "Fizz" 
+
+    //individua gli elementi divisibili per 5
+
+        // cambia il valore dei numeri divisibili per 3 in "Buzz" 
+
+    //individua gli elementi divisibili si per 3 che per 5 
+
+        //cambia il valore dei numeri divisibili per 3 e 5 in "FizzBuzz" 
+
+// creiamo delle box che racchiudano i numeri
+
+//carichiamo il risultato in pagina
