35dd1e3899cf2777d171404d3c4a477a23552bd3
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-06 19:10:39 +0200

    Caricamento in console dei numeri e del FizzBuzz

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 // Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
-// creare un circolo che generi i numeri da 1 a 100
+// creare un ciclo che generi i numeri da 1 a 100
 
-    //individua i numeri divisibili per 3
+for (let i = 1; i <= 100; i++ ) {
+    //individua gli elementi divisibili si per 3 che per 5 e cambia il valore dei numeri trovati in "FizzBuzz"
+    if (i%3 == 0 && i%5 == 0) {
+        console.log("FizzBuzz")
 
-        // cambia il valore dei numeri divisibili per 3 in "Fizz" 
+    //individua gli elementi divisibili per 3 e cambia il valore dei numeri trovati in "Fizz" 
 
-    //individua gli elementi divisibili per 5
+    //individua gli elementi divisibili per 5 e cambia il valore dei numeri trovati in "Buzz"
 
-        // cambia il valore dei numeri divisibili per 3 in "Buzz" 
-
-    //individua gli elementi divisibili si per 3 che per 5 
-
-        //cambia il valore dei numeri divisibili per 3 e 5 in "FizzBuzz" 
-
+    } else {
+        console.log(i);
+    }
 // creiamo delle box che racchiudano i numeri
+}
 
 //carichiamo il risultato in pagina
